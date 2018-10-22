30f52bafac21e5c3d4899a60424739771275b4bf
jsunwoo <jinsung.sunwoo@gmail.com>
2018-10-22 14:45:20 +0900

    Create MoviePoster component

---
+++ src/Movie.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./Movie.css";
+
+class Movie extends Component {
+  render() {
+    return (
+      <div>
+        <MoviePoster />
+        <h1>hello this is a movie</h1>
+      </div>
+    );
+  }
+}
+
+class MoviePoster extends Component {
+  render() {
+    return (
+      <img src="https://lajoyalink.com/wp-content/uploads/2018/03/Movie.jpg" />
+    );
+  }
+}
+
+export default Movie;
