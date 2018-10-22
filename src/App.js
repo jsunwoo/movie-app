db975fcdf7cccc71f75190997b29c3a9d74a0cc4
jsunwoo <jinsung.sunwoo@gmail.com>
2018-10-22 15:02:05 +0900

    How to use props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
+const Movies = ["Matrix", "Full Metal Jacket", "Oldboy", "Star Wars"];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
-        <Movie />
-        <Movie />
+        <Movie title={Movies[0]} />
+        <Movie title={Movies[1]} />
+        <Movie title={Movies[2]} />
+        <Movie title={Movies[3]} />
       </div>
     );
   }
