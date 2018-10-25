e5a92779587dbe0d90ed82ef14508d3f47a8f1f1
jsunwoo <jinsung.sunwoo@gmail.com>
2018-10-25 14:59:39 +0900

    Fix the issue (can't resolve 'babel-loader') by restarting yarn start

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./Movie.css";
 
 class Movie extends Component {
+  static propTypes = {
+    title: PropTypes.string
+  };
+
   render() {
     //console.log(this.props);
     return (
