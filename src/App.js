986b67b4b28dce879a17283f94f486fd3810a0e5
jsunwoo <jinsung.sunwoo@gmail.com>
2018-10-26 23:53:43 +0900

    How to fetch URL

--- src/App.js
+++ src/App.js
@@ -6,37 +6,7 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            image:
-              "http://www.gstatic.com/tv/thumb/v22vodart/22804/p22804_v_v8_as.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            image:
-              "http://www.gstatic.com/tv/thumb/v22vodart/10114/p10114_v_v8_ab.jpg"
-          },
-          {
-            title: "Oldboy",
-            image:
-              "http://www.gstatic.com/tv/thumb/v22vodart/35948/p35948_v_v8_ab.jpg"
-          },
-          {
-            title: "Star Wars",
-            image:
-              "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
-          },
-          {
-            title: "Trainspotting",
-            image:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Trainspotting_ver2.jpg/250px-Trainspotting_ver2.jpg"
-          }
-        ]
-      });
-    }, 3000);
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating");
   }
 
   _renderMovies = () => {
