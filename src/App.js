dd6745bdeca3d1d490ef5d8360557addf111adb1
jsunwoo <jinsung.sunwoo@gmail.com>
2018-10-28 18:04:11 +0900

    How to use async, await

--- src/App.js
+++ src/App.js
@@ -6,15 +6,32 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
-      .then(apple => apple.json())
-      .then(banana => console.log(banana))
-      .catch(err => console.log(err));
+    this._getMovies();
   }
 
+  _getMovies = async () => {
+    const allmovie = await this._callApi();
+    this.setState({
+      movieState: allmovie
+    });
+  };
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      .then(apple => apple.json())
+      .then(banana => banana.data.movies)
+      .catch(err => console.log(err));
+  };
+
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} image={movie.image} key={index} />;
+    const movies = this.state.movieState.map(movie => {
+      return (
+        <Movie
+          title={movie.title}
+          image={movie.medium_cover_image}
+          key={movie.id}
+        />
+      );
     });
     return movies;
   };
@@ -22,7 +39,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.movies ? this._renderMovies() : "Loading..."}
+        {this.state.movieState ? this._renderMovies() : "Loading..."}
       </div>
     );
   }
